2f2689257c27c8370f9d17721e5121d857f3fadd
Ebrahim aharpour <ebrahim@openweb.nl>
2016-03-19 17:10:48 +0100

    the database connection works now. Also README.md was added.

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -14,7 +14,9 @@ DMP_OPTS="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${CATALINA_BASE}/logs
 RMI_OPTS="-Djava.rmi.server.hostname=127.0.0.1"
 JRC_OPTS="-Dorg.apache.jackrabbit.core.cluster.node_id=${CLUSTER_ID}"
 L4J_OPTS="-Dlog4j.configuration=file:${CATALINA_BASE}/conf/log4j.xml"
+DB_OPTS="-DDB_HOST=${DB_HOST} -DDB_PORT=${DB_PORT} -DDB_NAME=${DB_NAME} -DDB_USER=${DB_USER} -DDB_PASS=${DB_PASS}"
 
-CATALINA_OPTS="${JVM_OPTS} ${REP_OPTS} ${DMP_OPTS} ${RMI_OPTS} ${L4J_OPTS} ${JRC_OPTS}"
+
+CATALINA_OPTS="${JVM_OPTS} ${REP_OPTS} ${DMP_OPTS} ${RMI_OPTS} ${L4J_OPTS} ${JRC_OPTS} ${DB_OPTS}"
 
 export CATALINA_HOME CATALINA_BASE
