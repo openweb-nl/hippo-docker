e8b4303ec86f4e35f807c4b0aa0d9650b82891f5
Matthijs Tolkamp <matthijs@openweb.nl>
2023-09-02 15:59:23 +0200

    add endorsed java

--- image/bin/setenv.sh
+++ image/bin/setenv.sh
@@ -2,6 +2,7 @@
 CATALINA_HOME="/usr/local/tomcat"
 CATALINA_BASE="/usr/local/tomcat"
 CATALINA_PID="${CATALINA_BASE}/work/catalina.pid"
+JAVA_ENDORSED_DIRS=${CATALINA_HOME}/endorsed
 
 if [[ "${CONSISTENCY_CHECK}" == "check" ]]
 then
